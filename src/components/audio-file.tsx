5015106662142341f819e73ca2a35017456a83ff
KG-11 <krishna.guru09@gmail.com>
2024-10-07 01:27:45 +0530

    Synced video seek to current time🙂

--- src/components/audio-file.tsx
+++ src/components/audio-file.tsx
@@ -1,10 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import ReactPlayer from "react-player";
 import { Button } from "@/components/ui/button";
 import { Volume2, VolumeOff } from "lucide-react";
 
 const AudioFile = () => {
   const [muted, setMuted] = useState<boolean>(true);
+  const videoRef = useRef<ReactPlayer>(null);
+
+  const initSeek = () => {
+    const videoDuration = 5 * 60 * 60 + 28 * 60 + 15;
+    const now = new Date();
+    const secondsSinceMidnight =
+      now.getHours() * 60 * 60 + now.getMinutes() * 60 + now.getSeconds();
+    const seekTime = secondsSinceMidnight % videoDuration;
+    videoRef?.current?.seekTo(seekTime, "seconds");
+  };
+
   return (
     <>
       <Button
@@ -15,14 +26,16 @@ const AudioFile = () => {
         {muted ? <VolumeOff /> : <Volume2 />}
       </Button>
       <ReactPlayer
+        ref={videoRef}
         style={{ display: "none" }}
         url={"https://vimeo.com/1016810491"}
-        controls={true}
         width="100%"
         height="100%"
-        loop
+        onReady={initSeek}
         muted={muted}
-        playing={true}
+        loop
+        playing
+        controls
       />
     </>
   );
